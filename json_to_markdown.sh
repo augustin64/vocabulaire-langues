310035c743dcba61909151db3fc4bd1d56ffa485
augustin64 <augustin.lucas64@gmail.com>
2021-11-22 14:48:41 +0100

    Ajout des données vers l'index

--- json_to_markdown.sh
+++ json_to_markdown.sh
@@ -5,25 +5,32 @@ OUT="md"
 json_to_md () 
 {
 	filename=$(echo "$1" | awk -F/ '{print $NF}')
+	f="/tmp/$filename"
+	o="${2}/$(echo "/tmp/$1" | sed -e 's/.json/.md/g' | awk -F/ '{print $NF}')"
+	title=$(echo "$f" | awk -F/ '{print $NF}' | sed -e 's/.json//g' -e 's/_/ /g')
+
 	if [[ $(which jq) != *"not found" ]]; then
 		jq '.' "$1" --unbuffered > "/tmp/$filename"
 	else
 		cp "$1" "/tmp/$filename"
 	fi;
-	f="/tmp/$filename"
+
 	echo "Converting $1"
-	o="${2}/$(echo "/tmp/$1" | sed -e 's/.json/.md/g' | awk -F/ '{print $NF}')"
-	echo "$f" | awk -F/ '{print "# " $NF}' | sed -e 's/.json//g' -e 's/_/ /g' > "$o"
+	echo "# $title" > $o
 	while read line
 	do
 		if [[ "$line" != *"}"* && "$line" != *"{"* ]]; then
 			echo "$line" | sed -e 's/"//g' -e 's/,//g' | awk -F: '{print "| "$1 " |" $2 " |"}' >> "$o"
 		elif [[ "$line" == *":"*"{"* ]]; then
-			title=$(echo "$line" | sed -e 's/"//g' -e 's/{//g' -e 's/: //g')
-			echo -e "## $title\n|||\n|:---|:---|" >> "$o"
+			theme_title=$(echo "$line" | sed -e 's/"//g' -e 's/{//g' -e 's/: //g')
+			echo -e "## $theme_title\n|||\n|:---|:---|" >> "$o"
 		fi;
 	done < "$f"
 	rm "$f"
+	# Ajoute le fichier à la liste des listes si il n'y ait pas déjà
+	if [[ -e md/README.md && $(cat md/README.md) != *"$title"* ]]; then
+		echo "|[$title]($(echo $o | awk -F/ '{print $NF}'))|" >> "md/README.md"
+	fi;
 }
 
 
